88ec4eef9064f1136f6df8b3089683f2d4cb2bc0
Jens van de Wiel <jens.vdwiel@gmail.com>
2024-05-06 20:12:58 +0200

    update tag

--- handlers/handler_helloworld.go
+++ handlers/handler_helloworld.go
@@ -5,7 +5,7 @@ import "github.com/labstack/echo/v4"
 // HelloWorldHandle return a simple hello world message
 // @Summary      Return a simple hello world message
 // @Description  Return a simple hello world message
-// @Tags         HelloWorld
+// @Tags         v1
 // @Produce      plain
 // @Success      200  {object}  string
 // @Router       /v1/hello [get]
